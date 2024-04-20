09b1ce06a31262351b448b8430b29da21f2c6399
Anupam Kumar Jha <anupamkumarjha@Anupams-MacBook-Pro.local>
2024-04-21 00:18:28 +0530

    Removed comment

--- tests/sample_test.sql
+++ tests/sample_test.sql
@@ -1,7 +1,7 @@
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where total_amount < 0 to make the test fail.
 select
-    customer_id
+    count(*) as cnt
 from {{ ref('customers' )}}
-group by 1
-having total_amount > 0
+where customer_id is null
+having cnt < 0
