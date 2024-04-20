ff573e2725d27f22650f8e02ebdf45097dadc7ff
Anupam Kumar Jha <anupamkumarjha@Anupams-MacBook-Pro.local>
2024-04-20 15:57:58 +0530

    Removed comment

--- models/example/customers.sql
+++ models/example/customers.sql
@@ -10,47 +10,64 @@ with customer_info as (
 ),
 
 -- dataset to get the first order date for each customer
-first_order_date as (
+first_order as (
     select
-        user_id as customer_id,
-        min(order_date) as most_recent_order
+        -- user_id as customer_id,
+        min(order_date) as first_order
     from customers.customers.hevo_raw_orders
     group by user_id
 ),
 
--- dataset to get the last order date for each customer
-last_order_date as (
-    select
-        id,
-        user_id as customer_id,
-        max(order_date) as number_of_orders
-    from customers.customers.hevo_raw_orders
-    group by user_id
-),
+-- -- dataset to get the last order date for each customer
 
--- dataset to calculate the sum of all payments made by each customer
-customer_payments as (
-    select
-        id as customer_id,
-        order_id,
-        sum(amount) as customer_lifetime_value
-    from customers.customers.hevo_raw_payments
-    group by id
-)
+-- most_recent_order as (
+--     select
+--         user_id as customer_id,
+--         max(order_date) as most_recent_order
+--     from customers.customers.hevo_raw_orders
+--     group by user_id
+-- ),
+
+-- number_of_orders as (
+--     select
+--         count(id) as number_of_orders
+--     from customers.customers.hevo_raw_orders
+--     group by user_id
+-- ),
+
+-- -- dataset to calculate the sum of all payments made by each customer
+-- customer_lifetime_value as (
+--     select
+--         id as customer_id,
+--         order_id,
+--         sum(amount) as customer_lifetime_value
+--     from customers.customers.hevo_raw_payments
+--     group by user_id
+-- )
 
 -- Final query to join all the information and create the target table customers
+-- select
+--     ci.customer_id,
+--     ci.first_name,
+--     ci.last_name,
+--     fod.most_recent_order,
+--     lod.number_of_orders,
+--     cp.customer_lifetime_value
+-- from
+--     customer_info ci
+-- left join
+--     first_order_date fod on ci.customer_id = fod.customer_id
+-- left join
+--     last_order_date lod on ci.customer_id = lod.customer_id
+-- left join
+--     customer_payments cp on lod.id = cp.order_id
+
 select
     ci.customer_id,
     ci.first_name,
     ci.last_name,
-    fod.most_recent_order,
-    lod.number_of_orders,
-    cp.customer_lifetime_value
+    fod.first_order
 from
     customer_info ci
 left join
-    first_order_date fod on ci.customer_id = fod.customer_id
-left join
-    last_order_date lod on ci.customer_id = lod.customer_id
-left join
-    customer_payments cp on lod.id = cp.order_id
+    first_order fod on ci.customer_id = fod.user_id
