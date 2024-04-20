d8da081bf05d6eadbc911104ba91ef4b7b4aabba
Anupam Kumar Jha <anupamkumarjha@Anupams-MacBook-Pro.local>
2024-04-20 17:01:39 +0530

    Removed comment

--- models/example/customers.sql
+++ models/example/customers.sql
@@ -34,17 +34,19 @@ number_of_orders as (
         count(id) as number_of_orders
     from customers.customers.hevo_raw_orders
     group by user_id
-)
+),
 
 -- dataset to calculate the sum of all payments made by each customer
--- customer_lifetime_value as (
---     select
---         id as customer_id,
---         order_id,
---         sum(amount) as customer_lifetime_value
---     from customers.customers.hevo_raw_payments
---     group by user_id
--- )
+
+customer_lifetime_value as (
+    select
+        sum(amount) as customer_lifetime_value,
+        order_id,
+        customers.customers.hevo_raw_orders.user_id as customer_id
+    from customers.customers.hevo_raw_payments
+    inner join customers.customers.hevo_raw_orders on customers.customers.hevo_raw_payments.order_id = customers.customers.hevo_raw_orders.id
+    group by customers.customers.hevo_raw_orders.user_id
+)
 
 select
     ci.customer_id,
@@ -52,7 +54,8 @@ select
     ci.last_name,
     first_order.first_order,
     most_recent_order.most_recent_order,
-    number_of_orders.number_of_orders
+    number_of_orders.number_of_orders,
+    customer_lifetime_value.customer_lifetime_value
 from
     customer_info as ci
 inner join
@@ -61,3 +64,5 @@ inner join
     most_recent_order on ci.customer_id = most_recent_order.customer_id
 inner join
     number_of_orders on ci.customer_id = number_of_orders.customer_id
+inner_join
+    customer_lifetime_value on ci.customer_id = customer_lifetime_value.customer_id
